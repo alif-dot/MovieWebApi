0d5b63c1e014d37ee0e00a5ae61597a42b0774d5
UY29 <UY29@DESKTOP-3OVPMB1>
2023-08-20 16:08:47 +0600

    Add project files.

---
+++ TestWebApi/Controllers/MoviesController.cs
@@ -0,0 +1,109 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MovieWebApi.Models;
+
+namespace MovieWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MovieContext _context;
+
+        public MoviesController(MovieContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Movie>>> GetMovies()
+        {
+            if(_context.Movies == null)
+            {
+                return NotFound();
+            }
+
+            return await _context.Movies.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Movie>> GetMovie(int id)
+        {
+            if(_context.Movies == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies.FindAsync(id);
+            if(movie == null)
+            {
+                return NotFound();
+            }
+            return movie;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Movie>> PostMovie(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetMovie), new {id = movie.Id}, movie);
+        }
+
+        [HttpPut("{id")]
+        public async Task<IActionResult> PutMovie(int id, Movie movie)
+        {
+            if(id != movie.Id)
+            {
+                return BadRequest();
+            }
+            
+            _context.Entry(movie).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!MovieExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteMovie(int id)
+        {
+            if(_context.Movies == null)
+            {
+                return NotFound();
+            }
+
+            var movie = await _context.Movies.FindAsync(id);
+            if(movie == null)
+            {
+                return NotFound();
+            }
+            
+            _context.Movies.Remove(movie);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool MovieExists(long id)
+        {
+            return (_context.Movies?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
